59fb1faf2edf501b4adc9dfecd89859c44e2f2d3
Aashish Dhawan <aashishdhawan@gmail.com>
2021-02-06 10:44:57 +0530

    Set Devise for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'static/home'
+  devise_for :users
+  get "static/home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static#home"
+
+  if %w(development).include?(Rails.env) && defined?(LetterOpenerWeb)
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
