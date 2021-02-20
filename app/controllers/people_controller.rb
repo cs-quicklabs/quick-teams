8095623669d3ffc0b940e0a2bef4035ff50ab481
Aashish Dhawan <aashishdhawan@gmail.com>
2021-02-20 15:20:47 +0530

    Allow User edit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,7 @@
 class PeopleController < ApplicationController
+  before_action :set_employee, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
   def index
     @employees = UserDecorator.decorate_collection(User.all)
   end
@@ -7,6 +10,29 @@ class PeopleController < ApplicationController
     @employee = User.new
   end
 
+  def update
+    respond_to do |format|
+      if @employee.update(employee_params)
+        format.html { redirect_to people_path, notice: "User was successfully updated." }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@employee, partial: "people/forms/form", locals: { employee: @employee }) }
+      end
+    end
+  end
+
   def create
+  end
+
+  def show
+  end
+
+  private
+
+  def set_employee
+    @employee = User.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:user).permit(:first_name, :last_name, :email, :role_id, :discipline_id, :job_id)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -58,12 +58,10 @@ class ProjectsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_project
     @project = Project.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def project_params
     params.require(:project).permit(:name, :description, :discipline_id)
   end
