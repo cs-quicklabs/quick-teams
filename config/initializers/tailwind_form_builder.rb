7d066c4542a60f0b4457402780c3370b10838b10
Aashish Dhawan <aashishdhawan@gmail.com>
2021-03-03 10:29:58 +0530

    Refactored UI in project screens

--- config/initializers/tailwind_form_builder.rb
+++ config/initializers/tailwind_form_builder.rb
@@ -1,10 +1,4 @@
 class TailwindFormBuilder < ActionView::Helpers::FormBuilder
-  # def text_field(attribute, options = {})
-  #   @template.content_tag :div, class: "mt-1" do
-  #     super(attribute, options.reverse_merge(class: "text-input"))
-  #   end
-  # end
-
   %w[text_field text_area password_field rich_text_area].each do |method_name|
     define_method(method_name) do |name, title, *args|
       @template.content_tag :div do
