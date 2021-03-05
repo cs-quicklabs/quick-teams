650738fe547d93a5a2b40b8dbfcddbde95c69898
Aashish Dhawan <aashishdhawan@gmail.com>
2021-03-05 19:57:28 +0530

    Fix id warning in form

--- config/initializers/tailwind_form_builder.rb
+++ config/initializers/tailwind_form_builder.rb
@@ -1,12 +1,34 @@
 class TailwindFormBuilder < ActionView::Helpers::FormBuilder
-  %w[text_field text_area password_field rich_text_area].each do |method_name|
+  %w[text_area rich_text_area].each do |method_name|
     define_method(method_name) do |name, title, *args|
       @template.content_tag :div do
         label(name, title, class: "block text-sm font-medium text-gray-700") +
         (@template.content_tag :div, class: "mt-1" do
-          super(name, options.reverse_merge(class: "text-input", id: "one"))
+          super(name, options.reverse_merge(class: "text-input"))
         end)
       end
+    end
+  end
+
+  def text_field(method, title, opts = {})
+    default_opts = { class: "text-input #{"border-2 border-red-00" if @object.errors.any?}" }
+    merged_opts = default_opts.merge(opts)
+    @template.content_tag :div do
+      label(method, title, class: "block text-sm font-medium text-gray-700") +
+      (@template.content_tag :div, class: "mt-1" do
+        super(method, merged_opts)
+      end)
+    end
+  end
+
+  def password_field(method, title, opts = {})
+    default_opts = { class: "text-input #{"border-2 border-red-00" if @object.errors.any?}" }
+    merged_opts = default_opts.merge(opts)
+    @template.content_tag :div do
+      label(method, title, class: "block text-sm font-medium text-gray-700") +
+      (@template.content_tag :div, class: "mt-1" do
+        super(method, merged_opts)
+      end)
     end
   end
 end
