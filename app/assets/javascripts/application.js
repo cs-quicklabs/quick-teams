05317f021737a5ba22e13dccbe1b87c36e5fa662
Aashish Dhawan <aashishdhawan@gmail.com>
2021-03-13 18:32:10 +0530

    Added search controller

--- app/assets/javascripts/application.js
+++
@@ -1,2 +0,0 @@
-//= require jquery.easy-autocomplete
-//= require search/search
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,7 @@
 class SearchController < ApplicationController
-  before_action :force_json, only: :search
-
   def search
     like_keyword = "%#{params[:q]}%"
-
-    @projects = Project.where("name ILIKE ?", like_keyword).limit(3)
-    @employees = User.for_current_account.where("first_name ILIKE ?", like_keyword).limit(3)
-  end
-
-  private
-
-  def force_json
-    request.format = :json
+    @employees = User.for_current_account.where("first_name ILIKE ?", like_keyword).limit(5)
+    render layout: false
   end
 end
