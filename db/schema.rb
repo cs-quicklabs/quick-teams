a19f73d0e12d7c5b3e86f6b93b081b253ef82eff
Aashish Dhawan <aashishdhawan@gmail.com>
2021-03-25 21:20:43 +0530

    test: added test for account/client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_145906) do
+ActiveRecord::Schema.define(version: 2021_03_25_154727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2021_03_24_145906) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["account_id"], name: "index_clients_on_account_id"
-    t.index ["email"], name: "index_clients_on_email", unique: true
+    t.index ["email"], name: "index_clients_on_email"
   end
 
   create_table "disciplines", force: :cascade do |t|
---
+++ test/models/client_test.rb
@@ -0,0 +1,47 @@
+require "test_helper"
+
+class ClientTest < ActiveSupport::TestCase
+  setup do
+    ActsAsTenant.current_tenant = accounts(:crownstack)
+  end
+
+  test "should have account" do
+    ActsAsTenant.current_tenant = nil
+    assert_raises ActsAsTenant::Errors::NoTenantSet do
+      client = Client.new(name: "John")
+      client.save
+    end
+  end
+
+  test "should have name" do
+    client = Client.new
+    client.save
+    assert_not client.errors[:name].empty?
+  end
+
+  test "should have email" do
+    client = Client.new
+    client.save
+    assert_not client.errors[:email].empty?
+  end
+
+  test "should have email unique to tenant" do
+    client1 = Client.new(name: "Steve", email: "rogers@gmail.com")
+    assert client1.save!
+
+    assert_raises ActiveRecord::RecordInvalid do
+      client2 = Client.new(name: "Steve", email: "rogers@gmail.com")
+      client2.save!
+    end
+
+    ActsAsTenant.current_tenant = accounts(:infosys)
+
+    client3 = Client.new(name: "Steve", email: "rogers@gmail.com")
+    assert client3.save
+
+    assert_raises ActiveRecord::RecordInvalid do
+      client4 = Client.new(name: "Steve", email: "rogers@gmail.com")
+      client4.save!
+    end
+  end
+end
