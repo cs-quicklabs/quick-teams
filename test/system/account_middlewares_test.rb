13d8b20b11fde9bb66071ef5c1899f3d49e5c749
Aashish Dhawan <aashishdhawan@gmail.com>
2021-03-28 17:23:50 +0530

    test: Added account middleware system test

---
+++ test/system/account_middlewares_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class AccountMiddlewaresTest < ApplicationSystemTestCase
+  setup do
+    @account = accounts(:crownstack)
+    sign_in users(:regular)
+  end
+
+  test "loads projects correctly" do
+    visit projects_url(script_name: "/#{@account.id}")
+    assert_selector "h1", text: "Projects"
+  end
+
+  test "cannot find the account redirects to main page" do
+    visit projects_url(script_name: "/123456789")
+    assert_equal "/", current_path
+  end
+end
