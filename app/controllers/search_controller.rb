18d1e5156d6f26cbb1cc1f5d326271c13296c701
Aashish Dhawan <aashishdhawan@gmail.com>
2021-04-07 10:03:08 +0530

    refactor: order in navbar search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def search
     like_keyword = "%#{params[:q]}%"
-    @employees = User.for_current_account.where("first_name ILIKE ?", like_keyword).limit(3)
-    @projects = Project.active.where("name ILIKE ?", like_keyword).limit(3)
+    @employees = User.for_current_account.where("first_name ILIKE ?", like_keyword).limit(3).order(:first_name)
+    @projects = Project.active.where("name ILIKE ?", like_keyword).limit(3).order(:name)
 
     render layout: false
   end
