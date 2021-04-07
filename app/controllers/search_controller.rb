a7624655d3d15b62043cc199273660f89481b743
Aashish Dhawan <aashishdhawan@gmail.com>
2021-04-07 10:00:43 +0530

    feature: added project in nav bar search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def search
     like_keyword = "%#{params[:q]}%"
-    @employees = User.for_current_account.where("first_name ILIKE ?", like_keyword).limit(5)
+    @employees = User.for_current_account.where("first_name ILIKE ?", like_keyword).limit(3)
+    @projects = Project.active.where("name ILIKE ?", like_keyword).limit(3)
+
     render layout: false
   end
 end
