a2254f8ac473fb77cf47393724c073532f05a2aa
Aashish Dhawan <aashishdhawan@gmail.com>
2021-04-08 09:20:43 +0530

    test: added defination of search Test

---
+++ test/system/search_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class SearchTest < ApplicationSystemTestCase
+  setup do
+  end
+
+  test "can search for employees" do
+  end
+
+  test "can search for projects" do
+  end
+
+  test "can not search deactivated employess" do
+  end
+
+  test "can not search archived projects" do
+  end
+end
