b008f01c1ba5ed25240be9d254b44661cca6a9ab
Mayank Mishra <mayank.m@hexagondl.com>
2021-04-14 01:36:27 +0530

    WIP: Add schedule in employee - having errors.

---
+++ app/controllers/employee/schedule_controller.rb
@@ -0,0 +1,7 @@
+class Employee::ScheduleController < Employee::BaseController
+	def index
+		collection = Schedule.where(employee: @employee).includes({ project: :discipline })
+		@schedules = ScheduleDecorator.decorate_collection(collection)
+		@schedule = Schedule.new
+	end
+end
