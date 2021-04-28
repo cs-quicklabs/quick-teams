c929d7d3304e8670865df5098ef1438693ff2efb
Aashish Dhawan <aashishdhawan@gmail.com>
2021-04-28 12:53:18 +0530

    syntax fix

--- app/controllers/employee/timeline_controller.rb
+++ app/controllers/employee/timeline_controller.rb
@@ -3,9 +3,10 @@ class Employee::TimelineController < Employee::BaseController
     authorize @employee, :show_timeline?
 
     collection = []
-    if @employee.id == current_user.id
+    if @employee.id == current_user.id # do not show what I am doing
       collection = Event.where(eventable: @employee).or(Event.where(trackable: @employee)).order(created_at: :desc)
-    elsif collection = Event.where(user: @employee).or(Event.where(eventable: @employee)).or(Event.where(trackable: @employee)).order(created_at: :desc)
+    else
+      collection = Event.where(user: @employee).or(Event.where(eventable: @employee)).or(Event.where(trackable: @employee)).order(created_at: :desc)
     end
     @events = collection.includes(:eventable, :trackable, :user).decorate
     fresh_when @events
