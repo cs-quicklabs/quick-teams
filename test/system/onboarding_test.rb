47425e0159d8da66e22194367bfb772ae267ad8f
Aashish Dhawan <aashishdhawan@gmail.com>
2021-05-15 17:21:47 +0530

    UI: invite UI

---
+++ test/system/onboarding_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class OnboardingTest < ApplicationSystemTestCase
+  test "admin can login" do
+  end
+
+  test "member can login" do
+  end
+
+  test "lead can login" do
+  end
+
+  test "user can recover forgotten password" do
+  end
+
+  test "admin can invite emmployee" do
+  end
+
+  test "user can reset password from settings" do
+  end
+end
