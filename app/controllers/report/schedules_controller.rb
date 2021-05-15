cd82b159981a5e1559328a37fd536e24f4f29541
Aashish Dhawan <aashishdhawan@gmail.com>
2021-05-15 19:01:08 +0530

    UI: for schedule reports

--- app/controllers/report/schedules_controller.rb
+++ app/controllers/report/schedules_controller.rb
@@ -16,12 +16,12 @@ class Report::SchedulesController < Report::BaseController
 
   def available_next_month
     @employees = User.for_current_account.active.billable.includes({ schedules: :project }, :role, :discipline, :job).order(:job_id).decorate
-    @employees = @employees.select { |e| schedule_not_planned_after_month(e.schedules) }
+    @employees = @employees.select { |e| schedule_not_planned_after_month(e) }
   end
 
-  def schedule_not_planned_after_month(schedules)
-    return true if schedules.size == 0
-    planned_till = schedules.try(:maximum, :ends_at)
+  def schedule_not_planned_after_month(employee)
+    return false if employee.schedules.size == 0 or employee.overall_occupancy < 60 #selected occupied resources only
+    planned_till = employee.schedules.try(:maximum, :ends_at)
     return true if planned_till.nil?
 
     planned_till < Date.today + 30.days
