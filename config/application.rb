b663247ee59757065ce844cd91ffa94739ecfd3e
Aashish Dhawan <aashishdhawan@gmail.com>
2021-05-29 11:20:58 +0530

    added deflaor

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Skia
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.use AccountMiddleware
+    config.middleware.use Rack::Deflater
+
     config.autoload_paths << "#{Rails.root}/lib"
     config.autoload_paths << "#{Rails.root}/interactors"
     config.autoload_paths << "#{Rails.root}/presenters"
