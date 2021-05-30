82f84a1eae31f3a8acc14c1ea212baee41604677
Aashish Dhawan <aashishdhawan@gmail.com>
2021-05-30 11:43:21 +0530

    updated migration

--- db/migrate/20210525045622_add_confirmable_to_user.rb
+++ db/migrate/20210525045622_add_confirmable_to_user.rb
@@ -6,10 +6,11 @@ class AddConfirmableToUser < ActiveRecord::Migration[6.1]
       t.datetime :confirmation_sent_at
       t.string :unconfirmed_email
     end
-
-    User.all.each do |user|
-      user.confirmed_at = Time.now.utc
-      user.save!
+    ActsAsTenant.without_tenant do
+      User.all.each do |user|
+        user.confirmed_at = Time.now.utc
+        user.save!
+      end
     end
   end
 end
