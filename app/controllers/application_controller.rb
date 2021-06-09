653b765afe370cb637be7d9b5afe056b706ad3ae
Aashish Dhawan <aashishdhawan@gmail.com>
2021-06-09 15:57:00 +0530

    enable errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  # rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
-  # rescue_from ActsAsTenant::Errors::NoTenantSet, with: :user_not_authorized
-  # rescue_from Pundit::NotDefinedError, with: :record_not_found
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
+  rescue_from ActsAsTenant::Errors::NoTenantSet, with: :user_not_authorized
+  rescue_from Pundit::NotDefinedError, with: :record_not_found
 
   etag { heroku_version }
 
