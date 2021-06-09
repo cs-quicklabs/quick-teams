c40e09e4461318ce7a2f203855b3ec5bd359ec3b
Aashish Dhawan <aashishdhawan@gmail.com>
2021-06-09 14:29:10 +0530

    fix file path

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,7 +11,7 @@ end
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(File.expand_path("../sidekiq_scheduler.yml", __FILE__))
+    Sidekiq.schedule = YAML.load_file(File.expand_path("../../sidekiq_scheduler.yml", __FILE__))
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
 end
