b7be7be01b4de5584f40fe098c44d6b3b6b4cfd7
hansika17 <hansika.kalra171@gmail.com>
2021-06-29 18:59:30 +0530

    no result found added to all files

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -12,4 +12,7 @@ class Todo < ApplicationRecord
   scope :order_by_deadline, -> { order(deadline: :desc) }
   scope :order_by_created, -> { order(created_at: :desc) }
   scope :order_by_updated, -> { order(updated_at: :desc) }
+  after_create_commit {broadcast_prepend_to "todos"}
+  after_update_commit {broadcast_replace_to "todos"}
+  after_destroy_commit {broadcast_remove_to "todos"}
 end
