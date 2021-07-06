0d7e02baf95cba3603901a08bdd17994536efdb3
Aashish Dhawan <aashishdhawan@gmail.com>
2021-07-06 09:15:23 +0530

    Update project billable resources after schedule update

--- app/interactors/remove_schedule.rb
+++ app/interactors/remove_schedule.rb
@@ -3,11 +3,13 @@ class RemoveSchedule < Patterns::Service
     @schedule = schedule
     @actor = actor
     @employee = schedule.user
+    @project = schedule.project
   end
 
   def call
     begin
       remove_schedule
+      update_billable_resources
       add_event
       send_email
     rescue
@@ -23,6 +25,18 @@ class RemoveSchedule < Patterns::Service
     schedule.destroy
   end
 
+  def update_billable_resources
+    project.billable_resources = billable_resources
+    project.save!
+  end
+
+  def billable_resources
+    project.schedules.reduce(0.0) do |sum, schedule|
+      sum += schedule.billable ? (schedule.occupancy/100.0) : 0
+      sum
+    end
+  end
+
   def add_event
     employee.events.create(user: actor, action: "freed", action_for_context: "freed", trackable: schedule.project)
   end
@@ -35,5 +49,5 @@ class RemoveSchedule < Patterns::Service
     employee.email_enabled and employee.account.email_enabled and employee.sign_in_count > 0
   end
 
-  attr_reader :actor, :schedule, :employee
+  attr_reader :actor, :schedule, :employee, :project
 end
--- app/interactors/update_schedule.rb
+++ app/interactors/update_schedule.rb
@@ -10,6 +10,7 @@ class UpdateSchedule < Patterns::Service
   def call
     begin
       update_schedule
+      update_billable_resources
       add_event
       send_email
     rescue
@@ -25,6 +26,18 @@ class UpdateSchedule < Patterns::Service
     schedule.project = @project
     schedule.user = @employee
     schedule.save!
+  end
+
+  def update_billable_resources
+    project.billable_resources = billable_resources
+    project.save!
+  end
+
+  def billable_resources
+    project.schedules.reduce(0.0) do |sum, schedule|
+      sum += schedule.billable ? (schedule.occupancy/100.0) : 0
+      sum
+    end
   end
 
   def add_event
