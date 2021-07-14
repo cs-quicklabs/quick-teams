8c35a7da604a3b7df5edc1790b160b4f38b6a4d7
Aashish Dhawan <aashishdhawan@gmail.com>
2021-07-14 10:45:00 +0530

    Added skeleton permission test for project feedbacks/notes

--- test/system/permissions/project/feedbacks_test.rb
+++ test/system/permissions/project/feedbacks_test.rb
@@ -12,4 +12,31 @@ class ProjectFeedbacksTest < ApplicationSystemTestCase
   def page_url
     project_feedbacks_url(script_name: "/#{@account.id}", project_id: @project.id)
   end
+
+  test "admin can see project feedbacks with edit buttons" do
+  end
+
+  test "lead can not see project feedbacks" do
+  end
+
+  test "lead can not see project feedback details" do
+  end
+
+  test "member can not see project feedbacks" do
+  end
+
+  test "member can not see project feedback details" do
+  end
+
+  test "manager can see project feedbacks with edit buttons" do
+  end
+
+  test "manage can see project feedback detail page" do
+  end
+
+  test "manager can not see feedback from other projects" do
+  end
+
+  test "manager can not see feedback details of other project feedbacks" do
+  end
 end
--- test/system/permissions/project/notes_test.rb
+++ test/system/permissions/project/notes_test.rb
@@ -12,4 +12,25 @@ class ProjectNotesTest < ApplicationSystemTestCase
   def page_url
     project_notes_url(script_name: "/#{@account.id}", project_id: @project.id)
   end
+
+  test "admin can see project notes with edit buttons" do
+  end
+
+  test "lead can not see project notes" do
+  end
+
+  test "member can not see project notes" do
+  end
+
+  test "manager can see create project note" do
+  end
+
+  test "manager can see edit button for his notes" do
+  end
+
+  test "manager can not see edit button for someone elses notes" do
+  end
+
+  test "manger can not see notes other than his project notes" do
+  end
 end
