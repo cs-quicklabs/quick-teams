23ed2a88b52d367640148c0e04aad3c15f1051e1
Aashish Dhawan <aashishdhawan@gmail.com>
2021-07-18 16:35:23 +0530

    added permission tests for project todos

--- test/system/permissions/project/todos_test.rb
+++ test/system/permissions/project/todos_test.rb
@@ -5,7 +5,7 @@ class ProjectTodosTest < ApplicationSystemTestCase
     @employee = users(:regular)
     @account = @employee.account
     ActsAsTenant.current_tenant = @account
-    @project = projects(:unarchived)
+    @project = projects(:managed)
     sign_in @employee
   end
 
@@ -14,17 +14,47 @@ class ProjectTodosTest < ApplicationSystemTestCase
   end
 
   test "admin can see project todos" do
+    sign_out @employee
+    @employee = users(:super)
+    sign_in @employee
+    visit page_url
+    assert_selector "div#project-tabs", text: "Todos"
+    assert_selector "div#todo-list"
+    assert_selector "form#new_todo"
   end
 
   test "lead can not see project todos" do
+    sign_out @employee
+    @employee = users(:lead)
+    sign_in @employee
+    visit page_url
+    assert_no_selector "div#project-tabs", text: "Todos"
   end
 
   test "member can not see project todos" do
+    sign_out @employee
+    @employee = users(:member)
+    sign_in @employee
+    visit page_url
+    assert_no_selector "div#project-tabs", text: "Todos"
   end
 
   test "manager can see project todos" do
+    sign_out @employee
+    @employee = users(:manager)
+    sign_in @employee
+    visit page_url
+    assert_selector "div#project-tabs", text: "Todos"
+    assert_selector "div#todo-list"
+    assert_selector "form#new_todo"
   end
 
   test "manager can not see project todos of other projects" do
+    sign_out @employee
+    @employee = users(:manager)
+    @project = projects(:one)
+    sign_in @employee
+    visit page_url
+    assert_no_selector "div#project-tabs", text: "Todos"
   end
 end
