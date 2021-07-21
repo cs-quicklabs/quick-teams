a9918a0845cc39669f46fb4fd5a6386975c264ee
Aashish Dhawan <aashishdhawan@gmail.com>
2021-07-21 09:47:21 +0530

    added permisson for employee skills

--- test/system/permissions/employee/skills_test.rb
+++ test/system/permissions/employee/skills_test.rb
@@ -13,20 +13,56 @@ class EmployeeSkillsTest < ApplicationSystemTestCase
   end
 
   test "admin can see skills" do
+    sign_out @employee
+    @employee = users(:super)
+    sign_in @employee
+    visit page_url
+    assert_selector "div#employee-tabs", text: "Skills"
   end
 
   test "lead can see his skills" do
+    sign_out @employee
+    @employee = users(:lead)
+    sign_in @employee
+    visit page_url
+    assert_selector "div#employee-tabs", text: "Skills"
   end
 
   test "lead can see his subordinate skills" do
+    sign_out @employee
+    @lead = users(:lead)
+    sign_in @lead
+    @employee = @lead.subordinates.first
+    visit page_url
+    assert_selector "h1", text: @employee.decorate.display_name
+    assert_selector "div#employee-tabs", text: "Skills"
   end
 
   test "lead can not see skills of other employee" do
+    sign_out @employee
+    @lead = users(:lead)
+    sign_in @lead
+    @employee = users(:admin)
+    visit page_url
+    assert_selector "h1", text: @lead.decorate.display_name
+    assert_selector "div#employee-tabs", text: "Skills"
   end
 
   test "member can see his skills " do
+    sign_out @employee
+    @employee = users(:member)
+    sign_in @employee
+    visit page_url
+    assert_selector "div#employee-tabs", text: "Skills"
   end
 
   test "member can not see someone elsees skills" do
+    sign_out @employee
+    @member = users(:member)
+    sign_in @member
+    @employee = users(:admin)
+    visit page_url
+    assert_selector "h1", text: @member.decorate.display_name
+    assert_selector "div#employee-tabs", text: "Skills"
   end
 end
