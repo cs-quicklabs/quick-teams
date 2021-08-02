9cb2e450ac49959de9639ce21aef3f655fbfe1bf
Aashish Dhawan <aashishdhawan@gmail.com>
2021-08-02 10:34:39 +0530

    use https:// in email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => "www.skia.me" }
+  config.action_mailer.default_url_options = { :host => "https://www.skia.me" }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
