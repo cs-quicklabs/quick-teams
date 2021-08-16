3daafbd9936de8015a27ed90f772bdd34e5fc53a
Aashish Dhawan <aashishdhawan@gmail.com>
2021-08-16 08:06:11 +0530

    refactored some UI

--- app/models/nuggets_user.rb
+++ app/models/nuggets_user.rb
@@ -1,13 +1,5 @@
 class NuggetsUser < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :user
-    belongs_to :nugget
-    get_primary_key :id
-
-end
-=======
   belongs_to :user
   belongs_to :nugget
   get_primary_key :id
 end
->>>>>>> dee249faab0bae609391c25673c6d5d0920e4c6e
