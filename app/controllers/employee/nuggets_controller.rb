c2518b9d24c372b6d33129c1d2298aecd75a3c00
Aashish Dhawan <aashishdhawan@gmail.com>
2021-08-22 13:21:52 +0530

    fix employee UI

--- app/controllers/employee/nuggets_controller.rb
+++ app/controllers/employee/nuggets_controller.rb
@@ -1,21 +1,29 @@
 class Employee::NuggetsController < Employee::BaseController
+  before_action :set_nugget, only: [:show]
+
   def index
     authorize [@employee, Nugget]
     @nugget = Nugget.new
-    @id = params[:skill_id]
 
-    if !@id.presence
-      nuggets = @employee.nuggets.select("nuggets_users.read as read", "nuggets.*").includes(:skill).order(read: :ASC).order(created_at: :desc).uniq
-    else
-      nuggets = @employee.nuggets.select("nuggets_users.read as read", "nuggets.*").where(skill_id: @id).includes(:skill).order(read: :ASC).order(created_at: :desc).uniq
-    end
-
-    @pagy, @nuggets = pagy_nil_safe(params, nuggets, items: 20)
+    @pagy, @nuggets = pagy_nil_safe(params, filtered_nuggets, items: 20)
     render_partial("employee/nuggets/nugget", collection: @nuggets, cached: true)
   end
 
   def show
-    authorize [@employee, Nugget]
-    @nuggets = @employee.nuggets.select("nuggets_users.read as read", "nuggets.*").includes(:skill).find_by_id(params[:id])
+    authorize [@employee, @nugget]
+  end
+
+  def filtered_nuggets
+    unless params[:skill_id].present?
+      nuggets = @employee.nuggets.select("nuggets_users.read as read", "nuggets.*").includes(:skill).order(read: :ASC).order(created_at: :desc).uniq
+    else
+      nuggets = @employee.nuggets.select("nuggets_users.read as read", "nuggets.*").where(skill_id: @id).includes(:skill).order(read: :ASC).order(created_at: :desc).uniq
+    end
+  end
+
+  private
+
+  def set_nugget
+    @nugget = @employee.nuggets.select("nuggets_users.read as read", "nuggets.*").includes(:skill).find_by_id(params[:id])
   end
 end
