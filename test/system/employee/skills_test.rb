c408d89c191e79a7c9c37ecc0e1e23e606f0bd1b
hansika17 <hansika.kalra171@gmail.com>
2021-09-06 22:57:46 +0530

    test case completed

--- test/system/employee/skills_test.rb
+++ test/system/employee/skills_test.rb
@@ -37,33 +37,41 @@ class EmployeeSkillsTest < ApplicationSystemTestCase
     first("#remove-skill").click
     assert_no_selector "#employee-skills", text: "Ruby"
   end
+
   test "can visit show all skills if logged in" do
     visit page_url
-    find("div", id: "show-skills").click_link(" or show all skills")
+    find("div", id: "show-skills").click_link("or show all skills")
     take_screenshot
-    assert_selector "h1", text: "#{@employee.first_name} #{@employee.last_name}'s skills" 
+    assert_selector "h1", text: "#{@employee.first_name} #{@employee.last_name}'s skills"
   end
+
   test "can add and remove skills via checkbox" do
     visit show_skills_url
-      skill = Skill.first
+    skill = @employee.skills.first
 
-    if find("li", id: dom_id(skill)).checked
-      find("li", id: dom_id(skill)).uncheck
-       @employee.reload
-      aseert @employee.skills.include?(skill), false
-    else
-      find("li", id: dom_id(skill)).check
+    if skill
+      find(:css, id: dom_id(skill)).set(false)
+      sleep(0.1)
       @employee.reload
-      aseert @employee.skills.include?(skill), true
+      assert_equal @employee.skills.include?(skill), false
+      take_screenshot
     end
-    take_screenshot
+
+    skill = Skill.where.not(id: @employee.skill_ids).first
+    if skill
+      find(:css, id: dom_id(skill)).set(true)
+      sleep(0.1)
+      @employee.reload
+      assert_equal @employee.skills.include?(skill), true
+      take_screenshot
+    end
   end
+
   test "back button should return to employee skills" do
     visit show_skills_url
     click_on "Back"
-    assert_text "Employee Skills", true
-    take_screenshot   
-  end
-  
+    assert_current_path(page_url)
 
+    take_screenshot
+  end
 end
