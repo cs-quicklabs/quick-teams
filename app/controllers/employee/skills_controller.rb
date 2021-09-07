673de164ad3f8e8ec49f2c85f9b6281d0edc26c6
Aashish Dhawan <aashishdhawan@gmail.com>
2021-09-07 09:50:31 +0530

    fix issues in skills

--- app/controllers/employee/skills_controller.rb
+++ app/controllers/employee/skills_controller.rb
@@ -2,9 +2,7 @@ class Employee::SkillsController < Employee::BaseController
   before_action :set_user, only: %i[show_skills]
 
   def index
-  
     authorize [@employee, Skill]
-  
 
     @skill = Skill.new
     @skills = @employee.skills.order("lower(name) asc")
@@ -13,10 +11,11 @@ class Employee::SkillsController < Employee::BaseController
   def show_skills
     authorize [User, Skill]
     @skills = Skill.order("lower(name) asc")
-    @employee_skills = @employee.skills
   end
-    private
-    def set_user
+
+  private
+
+  def set_user
     @employee = User.find(params["employee_id"]).decorate
   end
 end
--- app/reflexes/employee_skills_reflex.rb
+++ app/reflexes/employee_skills_reflex.rb
@@ -13,14 +13,16 @@ class EmployeeSkillsReflex < ApplicationReflex
 
     morph "#employee-skills", render(partial: "employee/skills/skills", locals: { employee: employee, skills: employee.skills, user: current_user })
   end
+
   def select
     employee = User.find(element.dataset["employee-id"])
-    if employee.skills.map{|skill| skill.id}.include?(element.dataset["skill-id"].to_i)
-      employee.skills.destroy Skill.find(element.dataset["skill-id"]) 
-       else
-        employee.skills << Skill.find(element.dataset["skill-id"])
-       end
-    skills= Skill.order("lower(name) asc")
-    morph "#show-skills", render(partial: "employee/skills/list", locals: { employee: employee, employee_skills: employee.skills, user: current_user, skills: skills })
+    skill = Skill.find(element.dataset["skill-id"])
+    if employee.skills.include? skill
+      employee.skills.destroy skill
+    else
+      employee.skills << skill
+    end
+    skills = Skill.order("lower(name) asc")
+    morph "#{dom_id(skill)}", render(partial: "employee/skills/skill", locals: { employee: employee, skill: skill, selected: employee.skills.include?(skill) })
   end
 end
