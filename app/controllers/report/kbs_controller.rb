cc1a987db5b264703f31e2cd683a5b4b351ddba8
Aashish Dhawan <aashishdhawan@gmail.com>
2021-09-07 10:22:27 +0530

    fix issues in document report

--- app/controllers/report/kbs_controller.rb
+++ app/controllers/report/kbs_controller.rb
@@ -1,16 +1,16 @@
 class Report::KbsController < Report::BaseController
-    def index
-      authorize :report
-  
-      kbs = Kb.includes(:user, :discipline, :job).query(kbs_filter_params, nil, created_at: :desc )
-    
-      @pagy, @kbs = pagy_nil_safe(kbs , items: LIMIT)
-      render_partial("report/kbs/kb", collection: @kbs, cached: false)
-    end
-    private
-  
-    def kbs_filter_params
-      params.permit(*KbFilter::KEYS)
-    end
+  def index
+    authorize :report
+
+    kbs = Kb.includes(:user, :discipline, :job).query(kbs_filter_params, nil, created_at: :desc)
+
+    @pagy, @kbs = pagy_nil_safe(params, kbs, items: LIMIT)
+    render_partial("report/kbs/kb", collection: @kbs, cached: false)
   end
-  
+
+  private
+
+  def kbs_filter_params
+    params.permit(*KbFilter::KEYS)
+  end
+end
