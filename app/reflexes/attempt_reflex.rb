03cadf723746b2754b2a9f00b752b7b17e7daaf7
Aashish Dhawan <aashishdhawan@gmail.com>
2021-09-12 10:18:38 +0530

    answer reflex

--- app/reflexes/attempt_reflex.rb
+++ app/reflexes/attempt_reflex.rb
@@ -5,7 +5,14 @@ class AttemptReflex < ApplicationReflex
     attempt = Survey::Attempt.find(element.dataset[:attempt_id])
     question = Survey::Question.find(element.dataset[:question_id])
     option = Survey::Option.find(element.dataset[:option_id])
-    Survey::Answer.create(attempt: attempt, question: question, option: option)
+
+    answer = Survey::Answer.find_by(attempt: attempt, question: question)
+    if answer
+      answer.update_attribute(:option_id, element.dataset[:option_id])
+    else
+      Survey::Answer.create(attempt: attempt, question: question, option: option)
+    end
+
     morph "#{dom_id(question)}", render(partial: "survey/attempts/checklist_question", locals: { attempt: attempt, question: question, option: option })
   end
 
