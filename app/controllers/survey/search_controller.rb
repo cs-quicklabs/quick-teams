4caf2f6bf1db639f07c840a1a3e4ccecd1bc29e3
Aashish Dhawan <aashishdhawan@gmail.com>
2021-09-16 11:05:12 +0530

    fix module name

--- app/controllers/survey/search_controller.rb
+++ app/controllers/survey/search_controller.rb
@@ -1,4 +1,4 @@
-class Surveys::SearchController < Surveys::BaseController
+class Survey::SearchController < Survey::BaseController
   def surveys
     like_keyword = "%#{params[:q]}%"
     @surveys = Survey::Survey.where("name ILIKE ?", like_keyword)
