028a896f2810f240b18020e9eeeb2521d511e3eb
hansika17 <hansika.kalra171@gmail.com>
2021-09-17 20:09:03 +0530

    fixes

--- app/models/survey/question_category.rb
+++ app/models/survey/question_category.rb
@@ -1,5 +1,9 @@
 class Survey::QuestionCategory < ApplicationRecord
   self.table_name = "survey_question_categories"
+  acts_as_tenant :account
+  has_many :questions, :dependent => :destroy
+  validates_presence_of :name
+  validates_uniqueness_to_tenant :name
 
   validates_presence_of :name
 end
