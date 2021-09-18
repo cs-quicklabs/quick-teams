02f2702d3104df26dc2ad0c9ce6c14a0c61a81fc
Aashish Dhawan <aashishdhawan@gmail.com>
2021-09-18 09:28:58 +0530

    added pagination to survey questions

--- app/controllers/survey/questions_controller.rb
+++ app/controllers/survey/questions_controller.rb
@@ -4,7 +4,7 @@ class Survey::QuestionsController < Survey::BaseController
 
   def index
     authorize [:survey, :question]
-    @pagy, @questions = pagy_nil_safe(params, Survey::Question.includes(:question_category).where(survey_id: @survey).order_by_category, items: LIMIT)
+    @pagy, @questions = pagy_nil_safe(params, Survey::Question.includes(:question_category, :survey).where(survey_id: @survey).order_by_category, items: LIMIT)
     @question_categories = Survey::QuestionCategory.all.order(:name)
     @question = Survey::Question.new
     render_partial("survey/questions/question", collection: @questions, cached: true) if stale?(@questions + @question_categories + [@survey])
