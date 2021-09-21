08e8f7bec420a53f3ce3407960d54b511e4baaea
hansika17 <hansika.kalra171@gmail.com>
2021-09-21 19:09:58 +0530

    kpi report

--- app/models/survey/survey.rb
+++ app/models/survey/survey.rb
@@ -40,6 +40,8 @@ class Survey::Survey < ActiveRecord::Base
     return !((current_number_of_attempts >= upper_bound) && (upper_bound != 0))
   end
 
+
+
   def clone_for_actor(actor)
     clone = Survey::Survey.new
     clone.name = self.name + " (Copy)"
