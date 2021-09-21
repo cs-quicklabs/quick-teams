7a1626819bfe37a00e981b504e3ef325f3e865dd
Aashish Dhawan <aashishdhawan@gmail.com>
2021-09-21 10:30:41 +0530

    enable self assessment

--- app/policies/user/kpi_policy.rb
+++ app/policies/user/kpi_policy.rb
@@ -7,9 +7,16 @@ class User::KpiPolicy < User::BaseUserPolicy
     true
   end
 
+  def self_record?
+    employee = record.first
+    return false if employee.kpi.nil?
+    user.id == employee.id
+  end
+
   def record?
     employee = record.first
     return false if employee.kpi.nil?
-    user.admin? || user.subordinate?(employee)
+    return true if user.admin? and (user.id != employee.id)
+    user.subordinate?(employee)
   end
 end
