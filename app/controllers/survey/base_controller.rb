24822bc0dfdd1b0786c16de48483087fc44a03d7
hansika17 <hansika.kalra171@gmail.com>
2021-09-26 13:43:45 +0530

    WIP

--- app/controllers/survey/base_controller.rb
+++ app/controllers/survey/base_controller.rb
@@ -8,18 +8,6 @@ class Survey::BaseController < BaseController
   private
 
   def resolve_redirect_path(attempt)
-    if attempt.participant.class.name == "Project"
-      attempt.survey.kpi? ? project_kpis_path(attempt.participant) : project_surveys_path(attempt.participant)
-    elsif attempt.participant.class.name == "User"
-      attempt.survey.kpi? ? employee_kpis_path(attempt.participant) : employee_surveys_path(attempt.participant)
-    end
-  end
-
-  def set_survey
-    @survey ||= Survey::Survey.find(params[:id])
-  end
-
-  def resolve_redirect_path
     if @attempt.survey.survey_for == "project"
       @attempt.survey.kpi? ? project_kpis_path(@attempt.participant) : project_surveys_path(@attempt.participant)
     elsif @attempt.survey.survey_for == "user"
@@ -27,5 +15,9 @@ class Survey::BaseController < BaseController
     elsif @attempt.survey.survey_for == "adhoc"
       survey_attempts_path(@attempt.survey)
     end
+  end
+
+  def set_survey
+    @survey ||= Survey::Survey.find(params[:id])
   end
 end
