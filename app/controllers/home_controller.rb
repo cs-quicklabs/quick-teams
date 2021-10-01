d451772027e509b474fb8e7d0c0db482d8df08f3
Aashish Dhawan <aashishdhawan@gmail.com>
2021-10-01 12:00:36 +0530

    fix n+1 queries on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < BaseController
   def goals
     authorize :home, :index?
 
-    @goals = Goal.next_90_days.order(status: :asc).order(deadline: :asc).limit(40)
+    @goals = Goal.next_90_days.includes(:user, :goalable).order(status: :asc).order(deadline: :asc).limit(40)
   end
 
   def events
