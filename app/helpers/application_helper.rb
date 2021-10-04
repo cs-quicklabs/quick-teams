8efa7895fcefd9838813adc49261f06b5f8bdd9f
Aashish Dhawan <aashishdhawan@gmail.com>
2021-10-04 21:59:14 +0530

    fix delete button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,18 +50,15 @@ module ApplicationHelper
   end
 
   def delete_button(path)
-    out = link_to "Delete", path, class: "btn-inline-delete", data: {
-                                    "turbo-method": :delete,
-                                    controller: "confirmation",
-                                    "confirmation-message-value": "Are you sure?",
-                                    action: "confirmation#confirm",
-                                  }
-
-    out.html_safe
+    confirm_button(path, "Delete", "Are you sure you want to delete this?", "btn-inline-delete")
   end
 
-  def confim_button(path, title, message)
-    out = link_to title, path, class: "btn-inline-delete", data: {
+  def styled_delete_button(path, style)
+    confirm_button(path, "Delete", "Are you sure you want to delete this?", style)
+  end
+
+  def confirm_button(path, title, message, style)
+    out = link_to title, path, class: style, data: {
                                  controller: "confirmation",
                                  "confirmation-message-value": message,
                                  action: "confirmation#confirm",
@@ -78,17 +75,6 @@ module ApplicationHelper
 
   def styled_edit_button(path, style)
     out = link_to "Edit", path, class: style
-
-    out.html_safe
-  end
-
-  def styled_delete_button(path, style)
-    out = link_to "Delete", path, class: style, data: {
-                                    "turbo-method": :delete,
-                                    controller: "confirmation",
-                                    "confirmation-message-value": "Are you sure?",
-                                    action: "confirmation#confirm",
-                                  }
 
     out.html_safe
   end
