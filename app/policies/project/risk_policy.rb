a1273858acbdb7cbad8155f8ab12e62e9e47f24c
hansika17 <hansika.kalra171@gmail.com>
2021-10-08 18:06:29 +0530

    policy added

--- app/policies/project/risk_policy.rb
+++ app/policies/project/risk_policy.rb
@@ -1,21 +1,28 @@
 class Project::RiskPolicy < Project::BaseProjectPolicy
   def update?
-    true
+    project = record.first
+    note = record.last
+    (user.admin? or note.user_id == user.id) and not project.archived?
   end
 
   def index?
-    true
+    project = record.first
+    return true if user.admin?
+    user.is_manager?(project)
   end
 
   def create?
-    true
+    project = record.first
+    return false if project.archived?
+    return true if user.admin?
+    user.is_manager?(project)
   end
 
   def destroy?
-    true
-  end
-
-  def edit?
-    true
+    project = record.first
+    note = record.last
+    return false if project.archived?
+    return true if user.admin?
+    note.user_id == user.id
   end
 end
