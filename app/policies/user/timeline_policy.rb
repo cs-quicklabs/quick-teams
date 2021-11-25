fc36d41ea9c170ffa46a92cc29d73d4e810e24e4
hansika17 <hansika.kalra171@gmail.com>
2021-11-25 11:30:10 +0530

    issue resolved

--- app/policies/user/timeline_policy.rb
+++ app/policies/user/timeline_policy.rb
@@ -1,10 +1,7 @@
 class User::TimelinePolicy < User::BaseUserPolicy
   def index?
-    employee = record.first
     user.admin?
      return true if user.on_project_team?(record.first)
-     return self_or_subordinate? if user.lead?
-    return false unless employee.active?
-    false
+
   end
 end
