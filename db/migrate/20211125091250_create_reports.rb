bba7093dd78882c73729281e201c1cd6a13822f7
Aashish Dhawan <aashishdhawan@gmail.com>
2021-12-04 18:35:05 +0530

    run migrations

--- db/migrate/20211125091250_create_reports.rb
+++ db/migrate/20211125091250_create_reports.rb
@@ -2,8 +2,8 @@ class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
       t.string :title
-      t.references  :reportable, polymorphic: true, null: false
-       t.boolean :submitted, default: false
+      t.references :reportable, polymorphic: true, null: false
+      t.boolean :submitted, default: false
       t.timestamps
     end
   end
