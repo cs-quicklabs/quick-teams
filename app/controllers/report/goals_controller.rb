df685ca2ed3737dd55816a26e26be9a0733f3b0b
Aashish Dhawan <aashishdhawan@gmail.com>
2021-12-04 14:46:57 +0530

    UI issues on goals

--- app/controllers/report/goals_controller.rb
+++ app/controllers/report/goals_controller.rb
@@ -9,25 +9,12 @@ class Report::GoalsController < Report::BaseController
     render_partial("report/goals/goal", collection: @goals.empty? ? @goals : @goals.includes(:goalable).decorate, cached: false)
   end
 
-   def open
+  def open
     authorize :report, :index?
-    
-    @ids = User.active.joins(:goals).where(:goals=>{:goalable_type=>'User', :deadline => Date.parse(params[:from_date].to_s)..Date.parse(params[:to_date].to_s)}).pluck('DISTINCT goalable_id')
-    no_goals = User.where.not(id: @ids)
-    @ids.each_with_index do |id,index|
-      count = Goal.where(goalable_type:'User',status:0, goalable_id:id, :deadline => Date.parse(params[:from_date].to_s)..Date.parse(params[:to_date].to_s)).group(:goalable_id).count
-        if count.empty?
-         @ids.delete_at(index)
-        end
-    end
-    employees = User.where(id: @ids).uniq
-    @employees= no_goals + employees
 
-    fresh_when @employees
+    @employees = User.active.joins(:goals)
+      .where.not(goals: { status: :progress }).uniq
   end
-
-
-
 
   private
 
