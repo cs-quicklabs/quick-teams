59feb240a0441a1411b41a8d5061c3db21d501fe
Aashish Dhawan <aashishdhawan@gmail.com>
2021-12-17 21:06:21 +0530

    added test case statement for survey attempts

--- test/system/permissions/employee/attempts_test.rb
+++ test/system/permissions/employee/attempts_test.rb
@@ -1,4 +1,44 @@
 require "application_system_test_case"
 
 class EmployeeSurveyAttemptsTest < ApplicationSystemTestCase
+  test "member can see his own attempts" do
+  end
+  test "member can not see other's attempts" do
+  end
+  test "member can see his own attempts previews" do
+  end
+  test "member can see his own attempt previews" do
+  end
+  test "admin can see his own attempts" do
+  end
+  test "admin can see other's attempts" do
+  end
+  test "admin can see his own attempt previews" do
+  end
+  test "admin can see all attempt previews" do
+  end
+  test "lead can see his own attempts" do
+  end
+  test "lead can not see other's attempts" do
+  end
+  test "lead can see his own attempts previews" do
+  end
+  test "lead can not see other's attempts previews" do
+  end
+  test "lead can see attempt previews of his subordinates" do
+  end
+  test "lead can not see attempt previews of other employees" do
+  end
+  test "project manager can see his own attempts" do
+  end
+  test "project manager can see his attempt previews" do
+  end
+  test "project manager can not see other's attempts" do
+  end
+  test "project manager can not see other's attempts previews" do
+  end
+  test "project manager can see attempt previews of his project participants" do
+  end
+  test "project manager can not see attempt previews of other project participant" do
+  end
 end
--- test/system/permissions/project/attempts_test.rb
+++ test/system/permissions/project/attempts_test.rb
@@ -1,4 +1,18 @@
 require "application_system_test_case"
 
 class ProjectSurveyAttemptsTest < ApplicationSystemTestCase
+  test "project manager can see all survey attempts for his project" do
+  end
+  test "project manager can not see survey attempts for other projects" do
+  end
+  test "admin can see all survey attempts for all project" do
+  end
+  test "member can not see survey attempts for any project" do
+  end
+  test "lead can not see survey attempts for any project" do
+  end
+  test "lead who is manager can see survey attempts of assigned project" do
+  end
+  test "lead who is manager can not see survey attempts of other project" do
+  end
 end
