5e048536ed4a0a23b8e15db06443c4537e5d34bb
Aashish Dhawan <aashishdhawan@gmail.com>
2021-12-20 10:10:20 +0530

    fix timesheets

--- app/policies/user/timesheet_policy.rb
+++ app/policies/user/timesheet_policy.rb
@@ -6,6 +6,7 @@ class User::TimesheetPolicy < User::BaseUserPolicy
   def edit?
     # user shoud be active and
     # admin, project manager or creator of timesheet can edit
+    timesheet = record.last
     is_active? and (is_admin? or is_project_manager? or (timesheet.user == user))
   end
 
