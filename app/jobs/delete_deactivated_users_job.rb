40a19edf02cf0c886b25214b7dc6a9b63975ac3d
Aashish Dhawan <aashishdhawan@gmail.com>
2022-01-05 10:38:48 +0530

    feat: added sidekiq job for deleting

--- app/jobs/delete_deactivated_users_job.rb
+++ app/jobs/delete_deactivated_users_job.rb
@@ -3,6 +3,10 @@ class DeleteDeactivatedUsersJob < ApplicationJob
     accounts = Account.all
     accounts.each do |account|
       ActsAsTenant.current_tenant = account
+      delete_deactivated_users_after_x_days = Preference.find_by(key: "delete_deactivated_users_after_x_days").value
+      if delete_deactivated_users_after_x_days != "-1" #Never is selected in settings, skip it
+        User.where("account_id = ? AND deactivated_on < ? AND active = false", account.id, delete_deactivated_users_after_x_days.to_i.days.ago).destroy_all
+      end
     end
   end
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -5,8 +5,8 @@ class Preference < ApplicationRecord
 
   def self.options_for(key)
     if key == "delete_timesheets_after_x_days"
-      [["1 week", "7"], ["1 month", "30"], ["3 months", "180"], ["1 year", "365"], ["Never", "-1"]]
-    elsif key == "delete_deactivated_user_after_x_days"
+      [["1 week", "7"], ["1 month", "30"], ["3 months", "90"], ["1 year", "365"], ["Never", "-1"]]
+    elsif key == "delete_deactivated_users_after_x_days"
       [["1 year", "365"], ["2 years", "730"], ["3 years", "1095"], ["Never", "-1"]]
     elsif key == "delete_archived_projects_after_x_days"
       [["1 year", "365"], ["2 years", "730"], ["3 years", "1095"], ["Never", "-1"]]
