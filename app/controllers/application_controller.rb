9502a28142a6ab5cbd394930989868801f723d2c
Aashish Dhawan <aashishdhawan@gmail.com>
2022-03-07 10:46:25 +0530

    revert: allow_other_host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,7 +65,7 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    redirect_to (request.referer || root_path), allow_other_host: true
+    redirect_to(request.referrer || landing_path)
   end
 
   def signed_in_root_path(resource)
