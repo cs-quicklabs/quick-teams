0bf4a0f1015db9b23e85f4f1d184eb311db2898f
Aashish Dhawan <aashishdhawan@gmail.com>
2022-03-14 10:57:34 +0530

    stripe using fake processor

--- app/interactors/sign_up.rb
+++ app/interactors/sign_up.rb
@@ -7,6 +7,7 @@ class SignUp < Patterns::Service
   def call
     begin
       register
+      set_stripe_subscription_trial
     rescue
       user
     end
@@ -55,6 +56,12 @@ class SignUp < Patterns::Service
         Preference.new(key: "consider_overall_kpi_score", value: "true", title: "Consider overall KPI score when KPIs are changed", message: "When KPIs are changed for an employee, do you wish to consider previous KPIs in overall score or just the new KPIs score should be considered while calculating final score").save
         Preference.new(key: "transfer_data_to_admin", value: User.where(account: account, permission: :admin).first.id, title: "Transfer data to admin on user delete", message: "When a user is deleted, you might want to keep some of the data like nuggets, Knowledge Base or Report Templates. Whom do you wish to assign this data when the user is deleted").save
       end
+    end
+
+    def set_stripe_subscription_trial
+      time = 14.days.from_now
+      user.set_payment_processor :fake_processor, allow_fake: true
+      user.payment_processor.subscribe(trial_ends_at: time, ends_at: time)
     end
   end
 
