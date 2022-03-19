801a370bd7228920b51bd3df728a5261db820533
hansika17 <hansika.kalra171@gmail.com>
2022-03-19 18:29:41 +0530

    ticket_label and ticket status handled

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
   has_many :todos, class_name: "Todo", foreign_key: "owner_id", dependent: :destroy
   has_many :created_todos, class_name: "Todo", foreign_key: "user_id", dependent: :destroy
   has_many :comments, class_name: "Comment", foreign_key: "user_id", dependent: :destroy
-  has_many :subscriptions, dependent: :destroy
+
 
   has_and_belongs_to_many :people_tags, dependent: :destroy
   has_and_belongs_to_many :skills, dependent: :destroy
