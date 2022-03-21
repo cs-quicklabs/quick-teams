31d61c7ad8fad9fcd3a75e4ec4ec30ab723d5b6b
hansika17 <hansika.kalra171@gmail.com>
2022-03-21 19:04:46 +0530

    team lead kpis enabled

--- app/controllers/employee/team_controller.rb
+++ app/controllers/employee/team_controller.rb
@@ -1,8 +1,16 @@
 class Employee::TeamController < Employee::BaseController
   def index
     authorize [@employee, Team]
-
+@manager=Array.new
     @subordinates = @employee.subordinates.includes(:role, :discipline, :job).order(:first_name)
+    @user=@employee
+    loop do
+        @manager.push(@user.manager) 
+         @user = @user.manager 
+         if !@user.manager.present?
+          break
+         end
+     end
     fresh_when @subordinates + [@employee]
   end
 end
