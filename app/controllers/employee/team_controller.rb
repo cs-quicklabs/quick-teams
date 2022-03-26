a69798389bd789cb6ebca84d91802f6029920ac4
Aashish Dhawan <aashishdhawan@gmail.com>
2022-03-26 10:40:03 +0530

    Fix UI for team leaders

--- app/controllers/employee/team_controller.rb
+++ app/controllers/employee/team_controller.rb
@@ -1,21 +1,8 @@
 class Employee::TeamController < Employee::BaseController
   def index
     authorize [@employee, Team]
-    @manager = Array.new
     @subordinates = @employee.subordinates.includes(:role, :discipline, :job).order(:first_name)
-    index = 0
-    if @employee.manager.present?
-      @user = @employee
-      until index > 1
-        @manager.push(@user.manager)
-        @user = @user.manager
-        index += 1
-        if !@user.manager.present?
-          break
-        end
-      end
-    end
+    @managers = @employee.managers.uniq
     fresh_when @subordinates + [@employee]
-    render_partial("employee/team/manager", collection: @manager) if stale?(@manager)
   end
 end
