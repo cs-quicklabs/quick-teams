0b07a378c5fc675a179aed4445872a216f0d8829
Aashish Dhawan <aashishdhawan@gmail.com>
2022-03-26 10:17:11 +0530

    running code linter

--- app/controllers/employee/team_controller.rb
+++ app/controllers/employee/team_controller.rb
@@ -1,19 +1,19 @@
 class Employee::TeamController < Employee::BaseController
   def index
     authorize [@employee, Team]
-@manager=Array.new
+    @manager = Array.new
     @subordinates = @employee.subordinates.includes(:role, :discipline, :job).order(:first_name)
-  index=0
+    index = 0
     if @employee.manager.present?
-    @user=@employee
-    until index>1
-        @manager.push(@user.manager) 
-         @user = @user.manager 
-    index+=1
-         if !@user.manager.present?
+      @user = @employee
+      until index > 1
+        @manager.push(@user.manager)
+        @user = @user.manager
+        index += 1
+        if !@user.manager.present?
           break
-         end
-     end
+        end
+      end
     end
     fresh_when @subordinates + [@employee]
     render_partial("employee/team/manager", collection: @manager) if stale?(@manager)
