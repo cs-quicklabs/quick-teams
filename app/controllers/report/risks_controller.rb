38b82d573051652aec28ab13130ff69aecf1df94
hansika17 <hansika.kalra171@gmail.com>
2022-04-12 20:16:34 +0530

    risk report fixed

--- app/controllers/report/risks_controller.rb
+++ app/controllers/report/risks_controller.rb
@@ -2,7 +2,7 @@ class Report::RisksController < Report::BaseController
   def index
     authorize :report, :index?
 
-    risks = Risk.open
+    risks = Risk.includes(:project, :user).where('projects.archived = ?', false).references(:projects).order("lower(name) ASC").open
     @pagy, @risks = pagy_nil_safe(params, risks, items: LIMIT)
     render_partial("report/risks/risk", collection: @risks, cached: false) if stale?(@risks)
   end
