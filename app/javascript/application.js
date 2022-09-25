c366844c7c62b040dbc64c15a36fc69070216a44
Aashish Dhawan <aashishdhawan@gmail.com>
2022-09-25 19:11:47 +0530

    update turbo-rails and stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo"
 import "./controllers"
 import 'trix'
 import '@rails/actiontext'
 import '@rails/activestorage'
 import '@tailwindcss/line-clamp'
 import '@tailwindcss/forms'
+import "@hotwired/turbo-rails"
