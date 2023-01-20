903834d773fbe00b5887fbe9fbb5ad6677d411df
Aashish Dhawan <aashishdhawan@gmail.com>
2023-01-20 12:49:08 +0530

    format controller.js

--- app/javascript/controllers/scroll_top_controller.js
+++ app/javascript/controllers/scroll_top_controller.js
@@ -1,19 +1,17 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  
-  scroll(event) {
-    event.preventDefault();
-    const target = document.querySelector(event.currentTarget.dataset.scrollTopTarget)
-    const elementPosition = target.getBoundingClientRect().top + window.scrollY;
-    window.scrollTo({
-      top: elementPosition,
-      left: 0,
-      behavior: 'smooth'
-    })
-  }
 
+    scroll(event) {
+        event.preventDefault();
+        const target = document.querySelector(event.currentTarget.dataset.scrollTopTarget)
+        const elementPosition = target.getBoundingClientRect().top + window.scrollY;
+        window.scrollTo({
+            top: elementPosition,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
 }
-
 
 
