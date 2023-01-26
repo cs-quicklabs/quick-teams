2962d9b0c35a9e8e8b23e6c0493f69b9c834e20d
Aashish Dhawan <aashishdhawan@gmail.com>
2023-01-26 11:11:06 +0530

    fix white ring

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,26 +1,26 @@
 module AvatarHelper
-  def display_xsmall_avatar(resource, ring = false)
-    display_avatar(resource, "w-7 h-7", "text-xs", ring)
+  def display_xsmall_avatar(resource)
+    display_avatar(resource, "w-7 h-7", "text-xs")
   end
 
-  def display_small_avatar(resource, ring = false)
-    display_avatar(resource, "w-8 h-8", "text-sm", ring)
+  def display_small_avatar(resource)
+    display_avatar(resource, "w-8 h-8", "text-sm")
   end
 
-  def display_medium_avatar(resource, ring = false)
-    display_avatar(resource, "w-10 h-10", "text-lg", ring)
+  def display_medium_avatar(resource)
+    display_avatar(resource, "w-10 h-10", "text-lg")
   end
 
-  def display_large_avatar(resource, ring = false)
-    display_avatar(resource, "w-40 h-40", "text-xl", ring)
+  def display_large_avatar(resource)
+    display_avatar(resource, "w-40 h-40", "text-xl")
   end
 
-  def display_avatar(resource, image_size, text_size, ring = false)
+  def display_avatar(resource, image_size, text_size)
     if resource.avatar.attached?
       image_tag resource.avatar, class: "rounded-full object-cover #{image_size}"
     else
       image = content_tag(:span, (resource.first_name[0, 1] + resource.last_name[0, 1]).upcase, class: "#{text_size} font-semibold text-white leading-none")
-      content_tag(:span, image.html_safe, class: "inline-flex items-center justify-center rounded-full bg-gray-500 ring-2 #{image_size} #{ring ? "ring-white" : "ring-white"}")
+      content_tag(:span, image.html_safe, class: "inline-flex items-center justify-center rounded-full bg-gray-500 ring-2 ring-white #{image_size}")
     end
   end
 end
