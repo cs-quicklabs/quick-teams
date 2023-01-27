df4ffb2ae7175e0b87f72cdade60d98f4ef64ad7
Aashish Dhawan <aashishdhawan@gmail.com>
2023-01-27 19:45:33 +0530

    fix N+1 Quesries

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -16,14 +16,11 @@ module AvatarHelper
   end
 
   def display_avatar(resource, image_size, text_size)
-    image = content_tag(:span, (resource.first_name[0, 1] + resource.last_name[0, 1]).upcase, class: "#{text_size} font-semibold text-white leading-none")
-    content_tag(:span, image.html_safe, class: "inline-flex items-center justify-center rounded-full bg-gray-500 ring-2 ring-white #{image_size}")
-
-    # if resource.avatar.attached?
-    #   image_tag resource.avatar, class: "rounded-full object-cover #{image_size}"
-    # else
-    #   image = content_tag(:span, (resource.first_name[0, 1] + resource.last_name[0, 1]).upcase, class: "#{text_size} font-semibold text-white leading-none")
-    #   content_tag(:span, image.html_safe, class: "inline-flex items-center justify-center rounded-full bg-gray-500 ring-2 ring-white #{image_size}")
-    # end
+    if resource.avatar.attached?
+      image_tag resource.avatar, class: "rounded-full object-cover #{image_size}"
+    else
+      image = content_tag(:span, (resource.first_name[0, 1] + resource.last_name[0, 1]).upcase, class: "#{text_size} font-semibold text-white leading-none")
+      content_tag(:span, image.html_safe, class: "inline-flex items-center justify-center rounded-full bg-gray-500 ring-2 ring-white #{image_size}")
+    end
   end
 end
