54d0013ccfeaaacdd1366a140de838959791d8ed
Aashish Dhawan <aashishdhawan@gmail.com>
2023-01-30 15:19:11 +0530

    profile pic default fixed

--- app/controllers/report/employees_controller.rb
+++ app/controllers/report/employees_controller.rb
@@ -2,7 +2,7 @@ class Report::EmployeesController < Report::BaseController
   def index
     authorize :report
 
-    entries = User.for_current_account.active.with_attached_avatar.includes(:status).query(employees_filter_params)
+    entries = User.for_current_account.active.includes(:status).query(employees_filter_params)
     @stats = EmployeesStats.new(entries)
 
     @pagy, @employees = pagy_nil_safe(params, entries, items: LIMIT)
