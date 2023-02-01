7a700bf81a78e7d5d0c8c551bf857b3176355238
Aashish Dhawan <aashishdhawan@gmail.com>
2023-02-01 11:46:51 +0530

    mail issue fixed but morph not working after that

--- app/reflexes/todo_reflex.rb
+++ app/reflexes/todo_reflex.rb
@@ -2,7 +2,11 @@ class TodoReflex < ApplicationReflex
   def toggle_project_todo
     todo = Todo.find(element.dataset[:id])
     todo.update(completed: !todo.completed)
-    TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).completed_email.deliver_later if deliver_email?(todo)
+    if deliver_email?(todo) && todo.completed
+      TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).completed_email.deliver_later
+    elsif deliver_email?(todo) && !todo.completed
+      TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).opened_email.deliver_later
+    end
     todo.save!
     morph "#{dom_id(todo)}", render(partial: "project/todos/todo", locals: { todo: todo })
   end
@@ -10,19 +14,29 @@ class TodoReflex < ApplicationReflex
   def toggle_todo
     todo = Todo.find(element.dataset[:id])
     todo.update(completed: !todo.completed)
-    TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).completed_email.deliver_later if deliver_email?(todo)
+    if deliver_email?(todo) && todo.completed
+      TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).completed_email.deliver_later
+    elsif deliver_email?(todo) && !todo.completed
+      TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).opened_email.deliver_later
+    end
     todo.save!
   end
 
   def toggle_employee_todo
     todo = Todo.find(element.dataset[:id])
     todo.update(completed: !todo.completed)
-    TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).completed_email.deliver_later if deliver_email?(todo)
-    todo.save!
-    morph "#{dom_id(todo)}", render(partial: "employee/todos/todo", locals: { todo: todo })
+    if deliver_email?(todo) && todo.completed
+      TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).completed_email.deliver_later
+      todo.save!
+      morph "#{dom_id(todo)}", render(partial: "employee/todos/todo", locals: { todo: todo })
+    elsif deliver_email?(todo) && !todo.completed
+      TodosMailer.with(actor: todo.user, employee: todo.owner, todo: todo).opened_email.deliver_later
+      todo.save!
+      morph "#{dom_id(todo)}", render(partial: "employee/todos/todo", locals: { todo: todo })
+    end
   end
 
   def deliver_email?(todo)
-    todo.completed and todo.user.email_enabled and todo.user.account.email_enabled and todo.user.sign_in_count > 0
+    todo.user.email_enabled and todo.user.account.email_enabled and todo.user.sign_in_count > 0
   end
 end
