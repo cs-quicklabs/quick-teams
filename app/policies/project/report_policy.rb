55baeceeb95182bda805348c21b30789c09b1b59
hansika17 <hansika.kalra171@gmail.com>
2023-02-15 16:15:41 +0530

    policies fixed in survey and report

--- app/policies/project/report_policy.rb
+++ app/policies/project/report_policy.rb
@@ -1,15 +1,15 @@
 class Project::ReportPolicy < Project::BaseProjectPolicy
   def edit?
     report = record.last
-    report.user == user and not report.submitted?
+    report.user == user and not report.submitted? and is_active?
   end
 
   def comment?
     report = record.last
-    is_admin? or is_project_manager?
+    is_admin? or is_project_manager? and is_active?
   end
 
   def destroy?
-    edit? or is_admin?
+    edit? and is_admin?
   end
 end
