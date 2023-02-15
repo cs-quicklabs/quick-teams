9a818291f5fa066423ffbd80c2f7856ab4f0f54d
Aashish Dhawan <aashishdhawan@gmail.com>
2023-02-15 18:51:23 +0530

    survey issues

--- app/policies/project/report_policy.rb
+++ app/policies/project/report_policy.rb
@@ -1,12 +1,14 @@
 class Project::ReportPolicy < Project::BaseProjectPolicy
   def edit?
+    return false if !is_active?
     report = record.last
-    report.user == user and not report.submitted? and is_active?
+    report.user == user and not report.submitted?
   end
 
   def comment?
+    return false if !is_active?
     report = record.last
-    is_admin? or is_project_manager? and is_active?
+    is_admin? or is_project_manager?
   end
 
   def destroy?
