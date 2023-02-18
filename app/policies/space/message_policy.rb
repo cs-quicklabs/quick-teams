5a79f21fc9652faff0ffa2ffb5f90a8825ce2851
Aashish Dhawan <aashishdhawan@gmail.com>
2023-02-18 13:48:17 +0530

    add title to list of users

--- app/policies/space/message_policy.rb
+++ app/policies/space/message_policy.rb
@@ -1,51 +1,50 @@
 class Space::MessagePolicy < Space::BaseSpacePolicy
   def index?
-    true
-    # space = record.first
-    # space.users.include?(user)
+    space = record.first
+    space.users.include?(user)
   end
 
-  # def new?
-  #   record.first.users.include?(user) && !record.first.archive
-  # end
+  def new?
+    record.first.users.include?(user) && !record.first.archive
+  end
 
-  # def create?
-  #   record.first.users.include?(user) && !record.first.archive
-  # end
+  def create?
+    record.first.users.include?(user) && !record.first.archive
+  end
 
-  # def show?
-  #   (record.last.published? && record.first.users.include?(user)) || record.last.user == user
-  # end
+  def show?
+    (record.last.published? && record.first.users.include?(user)) || record.last.user == user
+  end
 
-  # def comment?
-  #   (record.first.users.include?(user)) && !record.first.archive && record.last.published?
-  # end
+  def comment?
+    (record.first.users.include?(user)) && !record.first.archive && record.last.published?
+  end
 
-  # def edit?
-  #   record.first.users.include?(user)
-  # end
+  def edit?
+    record.first.users.include?(user)
+  end
 
-  # def update?
-  #   edit?
-  # end
+  def update?
+    edit?
+  end
 
-  # def destroy?
-  #   edit?
-  # end
+  def destroy?
+    edit?
+  end
 
-  # def publish?
-  #   record.first.users.include?(user) && !record.first.archive && !record.last.published?
-  # end
+  def publish?
+    record.first.users.include?(user) && !record.first.archive && !record.last.published?
+  end
 
-  # def delete_draft?
-  #   record.last.user == user && !record.last.published?
-  # end
+  def delete_draft?
+    record.last.user == user && !record.last.published?
+  end
 
-  # def draft?
-  #   record.first.users.include?(user) && !record.last.published?
-  # end
+  def draft?
+    record.first.users.include?(user) && !record.last.published?
+  end
 
-  # def edit_comment?
-  #   !record.first.archive?
-  # end
+  def edit_comment?
+    !record.first.archive?
+  end
 end
