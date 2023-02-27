3ceb96b9c234047d9e99332645e2704d5b2c284b
hansika17 <hansika.kalra171@gmail.com>
2023-02-27 16:55:09 +0530

    space users fixed

--- app/interactors/update_space.rb
+++ app/interactors/update_space.rb
@@ -23,11 +23,14 @@ class UpdateSpace < Patterns::Service
   def update_space_users
     space_users = space.users
     new_space_users = users - space_users.pluck(:id)
+    user = space_users.pluck(:id) - users
     if !new_space_users.empty?
       space_users << User.where("id IN (?)", new_space_users)
+    end
+    if !space_users.include?(actor)
       space_users << actor
     end
-    user = space_users.pluck(:id) - users
+
     if !user.empty?
       space_users.delete(User.where("id IN (?)", user))
     end
