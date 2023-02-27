51cac226192440f39e116bcfd0018af0e9e8daa6
hansika17 <hansika.kalra171@gmail.com>
2023-02-27 16:49:42 +0530

    space users fixed

--- app/models/spaces_user.rb
+++ app/models/spaces_user.rb
@@ -1,5 +1,5 @@
 class SpacesUser < ApplicationRecord
   belongs_to :user
   belongs_to :space
-  validates_uniqueness_of :user, scope: :space
+  validates_uniqueness_of :user_id, scope: :space_id
 end
