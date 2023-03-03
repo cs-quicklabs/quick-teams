8b610f99c880d0d2ab0a17248ce528b2b556a65a
Aashish Dhawan <aashishdhawan@gmail.com>
2023-03-03 19:50:48 +0530

    added upload limit on action text

--- app/javascript/trix-editor-overrides.js
+++ app/javascript/trix-editor-overrides.js
@@ -1,14 +1,14 @@
 window.addEventListener("trix-file-accept", function(event) {
-    const acceptedTypes = ['image/jpeg', 'image/png','application/pdf']
+    const acceptedTypes = ['image/jpeg','image/jpg', 'image/png','application/pdf']
     if (!acceptedTypes.includes(event.file.type)) {
       event.preventDefault()
-      alert("Only support attachment of jpeg or png files and pdf")
+      alert("Only JPEG, JPG, PNG and PDF files are supported!")
     }
   })
   window.addEventListener("trix-file-accept", function(event) {
     const maxFileSize =10*1024*1024
     if (event.file.size > maxFileSize) {
       event.preventDefault()
-      alert("Only support attachment files upto size 10MB!")
+      alert("Only supports attachment files upto size 10MB!")
     }
   })
