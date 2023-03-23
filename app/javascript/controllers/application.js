42910ddb377b80a2b041f58e4db124aea9026b03
Aashish Dhawan <aashishdhawan@gmail.com>
2023-03-23 07:37:59 +0530

    refactor stimulus reflex

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import consumer from "../channels/consumer"
+
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.warnings = true
-application.debug = false
-window.Stimulus = application
+application.debug    = false
+application.consumer = consumer
+window.Stimulus      = application
 
 export { application }
+
