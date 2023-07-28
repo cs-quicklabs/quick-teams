34b4c1111c11536f0cb717c2af99466a851e8111
hansika17 <hansika.kalra171@gmail.com>
2023-07-28 16:37:52 +0530

    highlight added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,3 +10,15 @@ import '@tailwindcss/forms'
 import "@hotwired/turbo-rails"
 import "./trix-editor-overrides"
 
+
+import Trix from "trix"
+
+Trix.config.textAttributes.highlight = { tagName: "mark" };
+
+addEventListener("trix-initialize", function (event) {
+    var groupElement = event.target.toolbarElement.querySelector(".trix-button-group.trix-button-group--text-tools")
+
+    groupElement.insertAdjacentHTML("beforeend", '<button type="button" class="trix-button trix-button--icon trix-button--icon-highlight" data-trix-attribute="highlight" data-trix-key="y" title="Highlight" tabindex="-1">${lang.highlight}')
+
+})
+
