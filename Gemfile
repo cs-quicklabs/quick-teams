6ba8f0d5e9789fafdc07cb799fd2049a8bec83a1
Aashish Dhawan <aashishdhawan@gmail.com>
2023-09-30 19:15:46 +0530

    update to rails 7.1.0.rc1

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.2.2"
 
 # Bundle edge Rails instead: gem 'rails', [https://github.com/rails/rails]
-gem "rails", "7.0.8"
+gem "rails", "7.1.0.rc1"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails", "3.4.2"
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module Skia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,7 +24,7 @@ module Skia
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.middleware.use AccountMiddleware
+config.middleware.use AccountMiddleware
     config.autoload_paths << "#{Rails.root}/lib"
     config.autoload_paths << "#{Rails.root}/interactors"
     config.autoload_paths << "#{Rails.root}/presenters"
