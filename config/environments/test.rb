b1737c931d5e3038a28f8846c123c2fe740b7c62
Aashish Dhawan <aashishdhawan@gmail.com>
2023-09-30 20:05:52 +0530

    running onboarding tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
   config.assets.css_compressor = nil
 
   # Raise error when a before_action's only/except options reference missing actions
- config.action_controller.raise_on_missing_callback_actions = true
+ config.action_controller.raise_on_missing_callback_actions = false
 end
