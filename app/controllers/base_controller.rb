52ab9dd212f0638515ea303aca60aec13f71d92b
Aashish Dhawan <aashishdhawan@gmail.com>
2025-12-02 19:03:40 +0530

    fix paggy

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -11,7 +11,7 @@ class BaseController < ApplicationController
 
   def pagy_nil_safe(params, collection, vars = {})
     pagy = Pagy.new(count: collection.count(:all), page: params[:page], **vars)
-    return pagy, collection.offset(pagy.offset).limit(pagy.items) if collection.respond_to?(:offset)
+    return pagy, collection.offset(pagy.offset).limit(pagy.limit) if collection.respond_to?(:offset)
     return pagy, collection
   end
 end
