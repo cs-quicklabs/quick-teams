33f9cf0bd2d5fa4a3ecfaefceee90c68e31bc468
Aashish Dhawan <aashishdhawan@gmail.com>
2025-12-04 19:59:43 +0530

    remove unwanted code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join("node_modules")
+# Rails.application.config.assets.paths << Rails.root.join("node_modules")
