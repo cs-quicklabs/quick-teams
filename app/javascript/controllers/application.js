e16f5b863b98766419ca7b45a594e74539a24ab5
Aashish Dhawan <aashishdhawan@gmail.com>
2025-12-06 18:16:38 +0530

    assets

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import consumer from "../channels/consumer"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.warnings = true
-application.debug    = false
-application.consumer = consumer
-window.Stimulus      = application
+application.debug = false
+window.Stimulus = application
 
 export { application }
