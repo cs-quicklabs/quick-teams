d3900816a974404c6d1c9e3fcd505c6f4a0e4675
Aashish Dhawan <aashishdhawan@gmail.com>
2025-12-06 17:50:38 +0530

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,6 @@
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Rails.root.join("node_modules")
+
+Rails.application.config.assets.paths << Rails.root.join("app/javascript")
+Rails.application.config.assets.paths << Rails.root.join("app/javascript/controllers")
