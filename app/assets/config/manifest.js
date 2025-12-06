3cbbe6193f4ba56061d3d7a57d283855a140fa67
Aashish Dhawan <aashishdhawan@gmail.com>
2025-12-06 17:41:27 +0530

    asset

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link slim.css
-//= link activestorage.esm.js
-//= link actiontext.esm.js
-//= link actioncable.esm.js
+//= link_tree ../stylesheets
