13001d44feeff8fffa516977ee75120b3c04cc7f
Aashish Dhawan <aashishdhawan@gmail.com>
2025-12-30 16:23:06 +0530

    fix redirect issue

--- app/controllers/survey/base_controller.rb
+++ app/controllers/survey/base_controller.rb
@@ -6,11 +6,11 @@ class Survey::BaseController < BaseController
   private
 
   def resolve_redirect_path(attempt)
-    if attempt.survey.survey_for == "project"
+    if attempt.survey.survey_for == 'project'
       attempt.survey.kpi? ? project_kpis_path(attempt.participant) : project_surveys_path(attempt.participant)
-    elsif attempt.survey.survey_for == "user"
-      attempt.survey.kpi? ? employee_kpis_path(attempt.participant) : employee_surveys_path(attempt.participant)
-    elsif attempt.survey.survey_for == "adhoc"
+    elsif attempt.survey.survey_for == 'user'
+      resolve_employee_page(attempt)
+    elsif attempt.survey.survey_for == 'adhoc'
       survey_attempts_path(attempt.survey)
     end
   end
@@ -18,14 +18,22 @@ class Survey::BaseController < BaseController
   def set_survey
     @survey ||= Survey::Survey.find(params[:id])
   end
+
+  def resolve_employee_page(attempt)
+    if attempt.survey.culture?
+      employee_culture_path(attempt.participant)
+    elsif attempt.survey.kpi?
+      employee_kpis_path(attempt.participant)
+    else
+      employee_surveys_path(attempt.participant)
+    end
+  end
 end
 
 class String
   def resolve_class
     klass = self
-    if self == "adhoc"
-      klass = "user"
-    end
+    klass = 'user' if self == 'adhoc'
     klass.capitalize.constantize
   end
 end
--- app/policies/user/culture_policy.rb
+++ app/policies/user/culture_policy.rb
@@ -6,12 +6,14 @@ class User::CulturePolicy < User::BaseUserPolicy
   def self_record?
     employee = record.first
     return false if employee.culture_kpi.nil?
+    
     user.id == employee.id
   end
 
   def record?
     employee = record.first
     return false if employee.culture_kpi.nil?
+
     is_active? and (is_admin? or is_project_manager? or is_team_lead? or is_project_observer?) and (user.id != employee.id)
   end
 end
